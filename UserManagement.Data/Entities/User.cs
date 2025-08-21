b2acbaaa4653a9acbb3db1600ecac62573b75d44
knyhago <kennethhagin545@gmail.com>
2025-08-21 21:02:24 +0100

    bug fix 2

--- UserManagement.Data/Entities/User.cs
+++ UserManagement.Data/Entities/User.cs
@@ -8,9 +8,26 @@ public class User
 {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
-    public string Forename { get; set; } = default!;
-    public string Surname { get; set; } = default!;
-    public DateOnly DateOfBirth { get; set; }
-    public string Email { get; set; } = default!;
+
+    [Required(ErrorMessage = "Forename is required")]
+    [MaxLength(25, ErrorMessage = "Forename cannot exceed 25 characters")]
+    [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "Forename can only contain letters")]
+    public string Forename { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Surname is required")]
+    [MaxLength(25, ErrorMessage = "Surname cannot exceed 25 characters")]
+    [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "Surname can only contain letters")]
+    public string Surname { get; set; } = string.Empty;
+
+
+    [Required(ErrorMessage = "Date of Birth is required")]
+
+    public DateOnly DateOfBirth { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email format")]
+    public string Email { get; set; } = string.Empty;
+
     public bool IsActive { get; set; }
 }
